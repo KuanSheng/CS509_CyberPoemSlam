732bd582352540fdf20cd58837cdd9f5f218b652
kuansheng <kuansheng@wifi-130-215-220-133.dyn.wpi.edu>
2014-10-09 18:36:17 -0400

    create word class

--- cs509_cyberPoetrySlam/src/Words/model/Word.java
+++ cs509_cyberPoetrySlam/src/Words/model/Word.java
@@ -1,5 +1,30 @@
 package Words.model;
 
 public class Word {
-
+	 int x;
+	 int y;
+	 
+     final String value;
+     final int width;
+     final int height;
+     
+     public Word(int x,int y,int width,int height,String value){
+    	 this.x = x;
+    	 this.y = y;
+    	 this.width = width;
+    	 this.height = height;
+    	 this.value = value;
+     }
+     
+     public int getX(){return this.x;}
+     public int getY(){return this.y;}
+     public int getWidth(){return this.width;}
+     public int getHeight(){return this.height;}
+     public String getValue(){return this.value;}
+     
+     public void setLocation(int x, int y ){
+    	 this.x = x;
+    	 this.y = y;
+     }
 }
+
