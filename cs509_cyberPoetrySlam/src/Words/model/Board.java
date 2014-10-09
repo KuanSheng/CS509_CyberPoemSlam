cb0ca0ee4c8b38ccd2eeb6578aadbb33fd389d3f
kuansheng <kuansheng@wifi-130-215-220-133.dyn.wpi.edu>
2014-10-09 18:56:25 -0400

    create class board

---
+++ cs509_cyberPoetrySlam/src/Words/model/Board.java
@@ -0,0 +1,11 @@
+package Words.model;
+import java.util.*;
+public class Board implements Iterable<Word>{
+
+	@Override
+	public Iterator<Word> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
