b721b4523cb16ee5fc55e1e95da2cf1fbd2d0650
YRZ <YRZ@130.215.125.176>
2014-10-14 17:28:45 -0600

    row to intersection

--- cs509_cyberPoetrySlam/src/Words/model/Row.java
+++ cs509_cyberPoetrySlam/src/Words/model/Row.java
@@ -49,6 +49,9 @@ public class Row extends Element{
 		// x left intersection and y no determined
 		if((this.x + this.width > word.x ) && (this.x + this.width < word.x + word.width) && 
 				(this.x < word.x)){
+			
+		
+			// y are equal then left intersection
 			if(this.y == word.y){     
 				intersection.type = 1;
 				intersection.e.type = 2;
@@ -56,14 +59,95 @@ public class Row extends Element{
 				intersection.e.y = word.y;
 			}
 			
-			else if((this.y < word.y) && (this.y > word.y - word.height)){
+			//row y overlaps with bottom part of word y
+			if((this.y < word.y) && (this.y > word.y - word.height)){
+				// y overlap more than x overlap then left intersection
 				if(this.y - (word.y - word.height) > (this.x + this.width - word.x)){
-					
+				intersection.type = 1;	
+				intersection.e.type = 2;
+				intersection.e.x = word.x - this.width;
+				intersection.e.y = word.y;		
+			}
+				// y overlap less than x overlap then bottom intersection
+				if(this.y - (word.y - word.height) < (this.x + this.width - word.x)){
+					intersection.type = 4;	
+					intersection.e.type = 3;
+					intersection.e.x = word.x;
+					intersection.e.y = word.y;	
 				}
+			}		
+					
+			// row y overlaps with top part of word y
+			if((this.y > word.y) && (this.y - this.width < word.y)){
+				//y overlap more than x overlap then left intersection
+				if( (word.y-(this.y - this.height)) > (this.x + this.width - word.x)){
+					intersection.type = 1;	
+					intersection.e.type = 2;
+					intersection.e.x = word.x - this.width;
+					intersection.e.y = word.y;
+				}
+				// y overlap less than x overlap then top intersection
+				if(word.y-(this.y - this.height)) < (this.x + this.width - word.x)){
+					intersection.type = 3;
+					intersection.e.type = 3;
+					intersection.e.x = word.x;
+					intersection.e.y = word.y + this.height;	
+				}	
 			}
 		}
 		
-		return intersection;
+		//x right intersection and y no determined
+		if((this.x < word.x + word.width ) && (this.x + this.width > word.x + word.width) && 
+				(this.x > word.x)){
+			
 		
+			// y are equal then right intersection
+			if(this.y == word.y){     
+				intersection.type = 2;
+				intersection.e.type = 2;
+				intersection.e.x = word.x;
+				intersection.e.y = word.y;
+			}
+			
+			//row y overlaps with bottom part of word y
+			if((this.y < word.y) && (this.y > word.y - word.height)){
+				// y overlap more than x overlap then right intersection
+				if(this.y - (word.y - word.height) > (word.x + word.width - this.x)){
+				intersection.type = 2;	
+				intersection.e.type = 2;
+				intersection.e.x = word.x;
+				intersection.e.y = word.y;		
+			}
+				// y overlap less than x overlap then bottom intersection
+				if(this.y - (word.y - word.height) < (word.x + word.width - this.x)){
+					intersection.type = 4;	
+					intersection.e.type = 3;
+					intersection.e.x = word.x;
+					intersection.e.y = word.y;	
+				}
+			}		
+					
+			// row y overlaps with top part of word y
+			if((this.y > word.y) && (this.y - this.width < word.y)){
+				//y overlap more than x overlap then right intersection
+				if( (word.y-(this.y - this.height)) > (word.x + word.width - this.x)){
+					intersection.type = 2;	
+					intersection.e.type = 2;
+					intersection.e.x = word.x;
+					intersection.e.y = word.y;
+				}
+				// y overlap less than x overlap then top intersection
+				if((word.y-(this.y - this.height)) < (word.x + word.width - this.x)){
+					intersection.type = 3;
+					intersection.e.type = 3;
+					intersection.e.x = word.x;
+					intersection.e.y = word.y + this.height;	
+				}	
+			}
+		}
+		
+		return intersection;	
 	}
+	
+	
 }
