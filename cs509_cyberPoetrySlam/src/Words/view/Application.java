97231f1f9373ac2e8c0923dbe9a26bafc4530401
Xinyuan Sun <evasun@EvatekiMacBook-Air.local>
2014-10-20 22:16:02 -0400

    Added a simple canvas panel

--- cs509_cyberPoetrySlam/src/Words/view/Application.java
+++ cs509_cyberPoetrySlam/src/Words/view/Application.java
@@ -7,13 +7,19 @@ import java.awt.event.ActionListener;
 import Words.model.Board;
 
 public class Application extends Frame {
+    // dummy main entrance
+    public static void main (String[] arg){
+        Board b = new Board();
+        Application app = new Application(b);
+    }
+
 	public Application(Board b){
 		super();
 		
-		setTitle("CyberPoetrySlam");
-		setSize(650,490);
-		
-		ApplicationCanvas panel = ApplicationCanvas(b);
-		add(panel);
+		ApplicationCanvas canvas = new ApplicationCanvas(b);
+        canvas.setTitle("CyberPoetrySlam");
+        canvas.setSize(1000, 800);
+        canvas.drawCanvas();
+		//add(panel);
 	}
 }
--- cs509_cyberPoetrySlam/src/Words/view/ApplicationCanvas.java
+++ cs509_cyberPoetrySlam/src/Words/view/ApplicationCanvas.java
@@ -1,14 +1,47 @@
 package Words.view;
 
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.Graphics;
+import java.awt.*;
 
 
 import Words.model.Board;
 
+import javax.swing.*;
+
 public class ApplicationCanvas extends Canvas{
 	Board board;
-	
+    public String canvasTitle;
+    public int canvasHeight;
+    public int canvasWidth;
+
+    // constructor
+    public ApplicationCanvas(Board board) {
+        this.board = board;
+    }
+
+    public void setTitle(String title) {
+        this.canvasTitle = title;
+    }
+
+    public void setSize(int h, int w){
+        this.canvasHeight = h;
+        this.canvasWidth = w;
+    }
+
+	public void drawCanvas() {
+        JFrame frame = new JFrame(this.canvasTitle);
+        frame.setSize(this.canvasHeight,this.canvasWidth);
+        GridLayout layout = new GridLayout(2, 1);
+        frame.setLayout(layout);
+
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(Color.ORANGE);
+
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.YELLOW);
+
+        frame.getContentPane().add(panel1);
+        frame.getContentPane().add(panel2);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
