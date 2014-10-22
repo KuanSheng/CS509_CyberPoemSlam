9c8b5f3035b2de4f575c1eb2e576dca4b87032d9
Xinyuan Sun <evasun@EvatekiMacBook-Air.local>
2014-10-21 22:13:04 -0400

    added some methods in board class

--- cs509_cyberPoetrySlam/src/Words/model/Board.java
+++ cs509_cyberPoetrySlam/src/Words/model/Board.java
@@ -1,7 +1,11 @@
 package Words.model;
 import java.util.*;
 public class Board implements Iterable<Word>{
-    ArrayList<Word> words = new ArrayList<Word>();
+    ArrayList<Word> protectedWord = new ArrayList<Word>();
+    ArrayList<Word> unprotectedWord = new ArrayList<Word>();
+    ArrayList<Poem> poemList = new ArrayList<Poem>();
+    ArrayList<Row> rowList = new ArrayList<Row>();
+
     //HashSet<String,Word> protectedWords = new HashSet<Word.value,Word>();
     public void restore(BoardMemento m) {
 		words = new ArrayList<Word>();
@@ -9,7 +13,30 @@ public class Board implements Iterable<Word>{
 			words.add(new Word(s.x, s.y, s.width, s.height, s.value,s.wordType));
 		}
 	}
-	
+
+    public void protect(Word w) {
+        // check if word is in unprotected word
+        if (unprotectedWord.contains(w)){
+            this.unprotectedWord.remove(w);
+            this.protectedWord.add(w);
+        }
+    }
+
+    public void unprotect(Word w) {
+        if(protectedWord.contains(w)){
+            this.protectedWord.remove(w);
+            this.unprotectedWord.add(w);
+        }
+    }
+
+	public void connect(Poem p, Row r, Word w){
+         
+    }
+
+    public void disconnect() {
+
+    }
+
     public BoardMemento getState() {
 		return new BoardMemento(words);
 	}
