d742d5add451ed6f9c5a0ae5ae1400fdc3df982f
YRZ <YRZ@192.168.1.134>
2014-10-21 21:18:31 -0600

    10/21_4

--- cs509_cyberPoetrySlam/src/Words/model/Poem.java
+++ cs509_cyberPoetrySlam/src/Words/model/Poem.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 
 public class Poem extends Element{
 	ArrayList<Word> words = new ArrayList<Word>();
+	ArrayList<Row> rows = new ArrayList<Row>();
+	
 	int x;
     int y;
+    int x_last;
+    int y_last;
     
     public Poem(int x, int y){
     	this.x = x;
