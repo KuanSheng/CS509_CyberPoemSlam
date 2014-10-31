1f31834dd16e57b0170fc9d9c34df2c16b825112
Xinyuan Sun <evasun@EvatekiMacBook-Air.local>
2014-10-30 20:12:34 -0400

    added test file

---
+++ cs509_cyberPoetrySlam/src/WordsTest/BoardTest.java
@@ -0,0 +1,91 @@
+package WordsTest;
+
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void testProtect() throws Exception {
+        assertTrue(true);
+
+    }
+
+    @org.junit.Test
+    public void testUnprotect() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddPoem() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRemovePoem() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetPoemList() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetPoemList() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetprotectedWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetprotectedWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetUnprotectedWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetUnprotectedWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetState() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testProtectWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testReleaseWords() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testProtectedWordsIterator() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUnprotectedWordssIterator() throws Exception {
+
+    }
+}
