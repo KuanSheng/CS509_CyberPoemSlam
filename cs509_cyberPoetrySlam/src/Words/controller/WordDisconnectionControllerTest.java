61bf7b755bfbd01ea27185ff42f2bbb04c8d362e
kuansheng <kuansheng@autoreg-671853.wifi.wpi.edu>
2014-11-06 16:01:33 -0500

    new branch task 2

---
+++ cs509_cyberPoetrySlam/src/Words/controller/WordDisconnectionControllerTest.java
@@ -0,0 +1,24 @@
+package Words.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordDisconnectionControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testWordDisconnectionController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisconnectEdgeWord() {
+		fail("Not yet implemented");
+	}
+
+}
