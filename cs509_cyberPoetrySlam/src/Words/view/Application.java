a638b4db2f701ef677aea5c896d9ada548912ecc
KuanSheng <ksheng@wpi.edu>
2014-11-30 15:24:44 -0500

    for merge

--- cs509_cyberPoetrySlam/src/Words/view/Application.java
+++ cs509_cyberPoetrySlam/src/Words/view/Application.java
@@ -1,66 +1,77 @@
+
 package Words.view;
 
+import java.io.*;
+import java.awt.event.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 
 import Words.controller.ReleasePoemController;
 import Words.controller.SubmitPoemController;
-import Words.model.Board;
-import Words.model.Model;
+import Words.model.*;
+;
 
 public class Application extends JFrame {
-     Model model;
+    Model model;
+    WordTable table;
+    WordTypeTable typeTable;
     private ApplicationCanvas panel; // added by JUN to share panel with StoreStateController
-
 	/**
 	 * This is the default constructor
 	 */
 //	public Application(Model m) { //Changed by JUN
     public Application(Model m, ApplicationCanvas panel){
 		super();
-        JPanel p = new JPanel();
-
-        //setLayout(new BorderLayout());
-        JPanel imagePanel = new JPanel();
-        imagePanel.setSize(650, 30);
-        setVisible(true);
-        JButton b1 = new JButton("Swap/Revoke");
-        JButton b2 = new JButton("Release");
-        JButton b3 = new JButton("Submit");
-        imagePanel.add(b1);
-        imagePanel.add(b2);
-        imagePanel.add(b3);
-
-
         this.model = m;
         this.panel = panel;
 
-        //add listener to "Release" button
-        b2.addActionListener(new ReleasePoemController(m, panel));
-        b3.addActionListener(new SubmitPoemController(m, panel, SubmitPoemController.Method.ALL));
+        setTitle("CyberPoetrySlam");
+        setSize(900, 900);
+        setLayout(new FlowLayout());
 
-		setTitle("CyberPoetrySlam");
-		setSize(650,490);
-        setResizable(true);
+        JPanel menuPanel = new JPanel();
+        menuPanel.setSize(900, 150);
         setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.orange);
+        JButton btnSwap = new JButton("Swap/Revoke");
+        JButton btnRelease = new JButton("Release");
+        JButton btnSubmit = new JButton("Submit");
+        JButton btnDisconnect = new JButton("Disconnect");
 
-		p.setSize(650,490);
-		p.setBackground(Color.yellow);
-		p.setBounds(0 ,245, 650, 345);
+        //add listener to "Release" button
+        btnRelease.addActionListener(new ReleasePoemController(m, panel));
+        btnSubmit.addActionListener(new SubmitPoemController(m, panel, SubmitPoemController.Method.ALL));
 
-		if(model == null)
-			return;
-		// mark as final so the anonymous class below can find it
-//		final ApplicationCanvas panel = new ApplicationCanvas(model); //commented by Jun
-		add(p);
-        p.add(imagePanel);
-        p.add(panel);
-	}
+        menuPanel.add(btnSwap);
+        menuPanel.add(btnRelease);
+        menuPanel.add(btnSubmit);
+        menuPanel.add(btnDisconnect);
+
+        Container pane = this.getContentPane();
+        //pane.setBackground(Color.yellow);
+        pane.add(menuPanel);
+
+        if(model == null) return;
+
+        // Where words appear
+        panel = new ApplicationCanvas(model);
+        panel.setSize(900, 500);
+        pane.add(panel);
+
+        // JTable on the side
+        JPanel tablePanel = new JPanel();
+        tablePanel.setSize(900, 250);
+
+        table = new WordTable(model.getBoard());
+        typeTable = new WordTypeTable(model.getBoard());
+        tablePanel.add(table);
+        tablePanel.add(typeTable);
+        pane.add(tablePanel);
+    }
 	
     public ApplicationCanvas getpanel(){
     	return this.panel;
     }
 }
+
+
